b0c2ec10defa10f971fb65d0b664ea64a473df62
Brahmprakash Gautam <112404933+bpgautam2003@users.noreply.github.com>
2023-05-22 15:58:02 +0530

    Add files via upload

---
+++ Node_API/data.js
@@ -0,0 +1,9 @@
+const data=[
+    {name:'Brahma',email:'abc@gmail.com'},
+    {name:'gautam',email:'xyz@gmail.com'},
+    {name:'anshu',email:'xyzwrs@gmail.com'},
+    {name:'shivansh',email:'jiudch@gmail.com'},
+    {name:'prakash',email:'xwed@gmail.com'}
+
+]
+module.exports=data;
